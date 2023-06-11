beb9e9631873723d6294941df6cc0838087bd34d
Tangcuyu <joysmshr@126.com>
2023-06-11 14:25:44 +0800

    feat(styles): change login ba\g

--- src/views/_builtin/login/components/login-bg/index.vue
+++ src/views/_builtin/login/components/login-bg/index.vue
@@ -1,28 +1,24 @@
 <template>
-  <div class="absolute-lt z-1 wh-full overflow-hidden">
-    <div class="absolute -right-300px -top-900px <sm:(-right-100px -top-1170px)">
-      <corner-top :start-color="lightColor" :end-color="darkColor" />
-    </div>
-    <div class="absolute -left-200px -bottom-400px <sm:(-left-100px -bottom-760px)">
-      <corner-bottom :start-color="darkColor" :end-color="lightColor" />
-    </div>
-  </div>
+  <div :style="{ backgroundColor: bgColor }"></div>
 </template>
 
 <script lang="ts" setup>
 import { computed } from 'vue';
+import { useThemeStore } from '@/store';
 import { getColorPalette } from '@/utils';
-import { CornerBottom, CornerTop } from './components';
 
 interface Props {
   /** 主题颜色 */
   themeColor: string;
 }
+const theme = useThemeStore();
 
 const props = defineProps<Props>();
 
-const lightColor = computed(() => getColorPalette(props.themeColor, 3));
+const lightColor = computed(() => getColorPalette(props.themeColor, 1));
 const darkColor = computed(() => getColorPalette(props.themeColor, 6));
+
+const bgColor = theme.darkMode ? darkColor : lightColor;
 </script>
 
 <style scoped></style>
--- src/views/_builtin/login/index.vue
+++ src/views/_builtin/login/index.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="relative flex-center wh-full" :style="{ backgroundColor: bgColor }">
+  <div class="relative flex-center wh-full" :style="{ backgroundColor: bgColor, backgroundImage: bgImage }">
     <dark-mode-switch
       :dark="theme.darkMode"
-      class="absolute left-48px top-24px z-3 text-20px"
+      class="absolute right-48px top-24px z-3 text-20px"
       @update:dark="theme.setDarkMode"
     />
     <n-card :bordered="false" size="large" class="z-4 !w-auto rounded-20px shadow-sm">
@@ -68,10 +68,13 @@ const activeModule = computed(() => {
 });
 
 const bgThemeColor = computed(() => (theme.darkMode ? getColorPalette(theme.themeColor, 7) : theme.themeColor));
+const bgImage = computed(() =>
+  theme.darkMode ? 'url(../public/g8_login_dark.svg)' : 'url(../public/g8_login_light.svg)'
+);
 
 const bgColor = computed(() => {
   const COLOR_WHITE = '#ffffff';
-  const ratio = theme.darkMode ? 0.5 : 0.2;
+  const ratio = theme.darkMode ? 0.5 : 0.1;
   return mixColor(COLOR_WHITE, theme.themeColor, ratio);
 });
 </script>
